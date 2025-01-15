cf3d719558497a3976f54baf1b54f5d830b6a6f1
ChrisEssomba <chris.essomba357@gmail.com>
2025-01-15 11:22:35 +0100

    Update app

--- App.py
+++ App.py
@@ -2,5 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello world ! "
+def hello():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
