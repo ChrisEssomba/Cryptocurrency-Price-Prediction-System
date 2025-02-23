b8ec6b3087e130424feba4771f09bd38bbfa0c8a
ChrisEssomba <chris.essomba357@gmail.com>
2025-02-23 14:30:56 +0100

    updates

--- App.py
+++ App.py
@@ -1,13 +1,58 @@
-from flask import Flask
+ #This example uses Python 2.7 and the python-request library.
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import pandas as pd
+import json
+
+
+from flask import Flask, render_template
+import requests
+import pandas as pd
+
 app = Flask(__name__)
 
+# API Data Fetching Function
+def get_data():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+    parameters = {
+    'start':'1',
+    'limit':'5000',
+    'convert':'USD'
+    }
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '9841adaf-41fd-4102-8637-124871313e7a',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        results = data.get('data', [])
+        
+        df = pd.DataFrame(results)
+        df = df.drop(columns=['tags', 'quote'])
+        #df.to_csv('output.csv')
+       #print(df)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e) 
+    return df
+
+# Flask Route for Home Page
 @app.route("/")
-def hello():
-    return "Hello world!"
+def home():
+    df = get_data()
+    
+    # Convert DataFrame to HTML Table
+    table_html = df.to_html(classes="table table-striped", index=False)
+    
+    return render_template("index.html", table=table_html)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(debug=True)
-=======
-    app.run(debug=True)
->>>>>>> 083b8de93e61807e173600626eaa7987447e0be1
+
+
