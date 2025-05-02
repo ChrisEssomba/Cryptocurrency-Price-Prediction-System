3a267aac78eafa30b3e4c1681a3409efcf9112b0
ChrisEssomba <123855534+ChrisEssomba@users.noreply.github.com>
2025-05-02 18:22:40 +0200

    Update create_aks_cluster.sh

--- create_aks_cluster.sh
+++ create_aks_cluster.sh
@@ -2,8 +2,8 @@
 
 # Variables
 RESOURCE_GROUP="CloudComputingIR4"
-#AKS_CLUSTER_NAME="crypto-cluster"
-ACR_NAME="cryto"
+AKS_CLUSTER_NAME="new-crypto-cluster"
+ACR_NAME="cryptocontain"
 #LOCATION="westeurope" #location west europe
 NODE_COUNT=1 #number of instance of the app that can be run (no need more than one)
 #NODE_VM_SIZE="Standard_B2" #smallest VM out there
@@ -12,21 +12,22 @@ NODE_COUNT=1 #number of instance of the app that can be run (no need more than o
 echo "Logging in to Azure..."
 az login --use-device-code
 
+'''
 # Create a resource group
 echo "Creating resource group '$RESOURCE_GROUP' in '$LOCATION'..."
 az group create --name $RESOURCE_GROUP 
-
+'''
 
 # Create an AKS cluster
 echo "Creating AKS cluster '$AKS_CLUSTER_NAME' with $NODE_COUNT nodes of size '$NODE_VM_SIZE'..."
-'''
+
 az aks create \
     --resource-group $RESOURCE_GROUP \
     --name $AKS_CLUSTER_NAME \
     --node-count $NODE_COUNT \
     --generate-ssh-keys
-'''
-az aks create --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME --node-count 1 --enable-managed-identity --enable-addons monitoring --generate-ssh-keys
+
+#az aks create --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME --node-count 1 --enable-managed-identity --enable-addons monitoring --generate-ssh-keys
 
 
 # Get AKS credentials
